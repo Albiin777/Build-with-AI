4a0ee87fbdb871341703d634b191d58c77f0fe74
Albin Thomas <albiin7777@gmail.com>
2026-04-05 19:06:35 +0530

    committed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const defaultConfig = {
   extraImages: [
-    "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?auto=format&fit=crop&w=1920&q=80"
+    "reopen (7).jpg.jpeg"
   ],
   imageHostingProvider: "imgbb",
   autoHostUploads: true,
